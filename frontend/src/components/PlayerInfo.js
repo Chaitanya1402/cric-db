5d7f491718f991100a25ef81d478b57c3d28afd4
Chaitanya <chaitanyas2002@gmail.com>
2022-08-16 15:29:05 +0530

    Used wikipedia api to get 5 lines about a player

--- frontend/src/components/PlayerInfo.js
+++ frontend/src/components/PlayerInfo.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PlayerInfo = (props) => {
+  const [wiki, setWiki] = useState("");
+
+  const wikiPlayer = async () => {
+    let url = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=5&explaintext=1&format=json&titles=${props.details.name}&origin=*`
+    const res = await fetch(url);
+    const json = await res.json();
+    // console.log(json.query.pages[Object.keys(json.query.pages)[0]].extract);
+    setWiki(json.query.pages[Object.keys(json.query.pages)[0]].extract)
+  }
+
+  useEffect(() => {
+    wikiPlayer();
+  })
+
   return (
     <>
-      <div className='w-[85%] m-auto'>
+      <div className='w-[75%] m-auto'>
         <div className='flex justify-between items-start'>
-          <div className='details w-[50%]'>
+          <div className='details w-[65%]'>
             <p className='text-4xl font-semibold'>{props.details.name}</p>
             <p className='text-xl font-medium'>{props.details.country}</p>
             <p className='text-lg mt-5'>Personal Information</p>
@@ -38,13 +52,17 @@ const PlayerInfo = (props) => {
 
             <p className='text-lg mt-5'>Teams played for:</p>
             
-            <div className='my-2 bg-gray-100 w-[60%] py-1 px-2'>
+            <div className='my-2 bg-gray-100 w-[60%] py-1 px-2 border border-gray-200'>
               {props.details.teamsPlayedFor}
             </div>
           </div>
           <div className="img">
             <img className='h-[30rem] object-cover' src={props.details.img} alt="Player" />
           </div>
+        </div>
+
+        <div className='my-5 text-justify'>
+          { wiki }
         </div>
       </div>
     </>
