4f2f148421fdb0388e82ddb23816b74d2239ed03
Chaitanya <chaitanyas2002@gmail.com>
2022-08-17 15:15:16 +0530

    explore more button arrows added.

--- frontend/src/components/Jumbotron.js
+++ frontend/src/components/Jumbotron.js
@@ -9,7 +9,11 @@ const Jumbotron = () => {
       </div>
       <div className='absolute top-0 rem-height pl-[7.5%] w-[55%] flex flex-col justify-center items-start'>
         <h2 className='block pb-10 text-6xl leading-[5rem] text-white font-anton'>Your one stop destination for cricket related information</h2>
-        <button className='text-white bg-purple-600 py-2 px-4 rounded-lg hover:bg-purple-700 more-button'>Explore More
+        <button className='text-white bg-purple-600 py-2 px-4 rounded-lg hover:bg-purple-700 more-button'>Explore More <span className='more-button-arrows pl-1'>
+          <span className='opacity-50'>&#10095;</span>
+          <span className='opacity-70'>&#10095;</span>
+          <span className='opacity-90'>&#10095;</span>
+        </span>
         </button>
       </div>
     </div>
