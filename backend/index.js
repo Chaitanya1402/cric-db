0b2c5fb3cb98f38cf14d7ce5b7be461ed75dbd9d
Chaitanya <chaitanyas2002@gmail.com>
2022-08-20 16:25:18 +0530

    sort player name in backend

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get("/api/players", (req, res) => {
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("cric-db");
-        dbo.collection("players").find({}).toArray(function(err, result) {
+        dbo.collection("players").find({}).sort({"name": 1}).toArray(function(err, result) {
             if (err) throw err;
             res.status(200);
             res.json({success: true, result: result});
